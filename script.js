9d40a298d811fa3db939bb1c38198ed4cdaff7ff
Gabriel Fabiano <gabrielfabianochaves@gmail.com>
2022-03-09 20:07:34 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,53 @@
+//comentarios
+/* 
+var mensagem = "Olá mundo!";
+alert(mensagem);
+
+var a = 2;
+var b = 3;
+var c = "Olá";
+
+var soma = a+b;
+var sub = a-b;
+var mult = a*b;
+var div = a/b;
+
+
+alert(soma);
+alert(sub);
+alert(mult);
+alert(div);
+
+if(c%2 == 1){
+    alert("Número impar");
+}else if(c%2 == 0){
+    alert("Número par");
+}
+else{
+    alert("Digite um valor válido!")
+}*/
+
+var i = 0;
+
+while(i < 3){
+    alert(i);
+
+    i = i + 1;
+}
+
+for(var j = 0; j < 3; j++){
+    alert(j);
+}
+
+var numero = 6;
+var decimal = 4.5;
+var palavra = "Olá mundo";
+var lista = ["Laranja", "Maçã","Banana"];
+
+alert(lista[0]);
+
+for(k in lista){
+    alert(lista[k]);
+}
+
+console.log("Olá mundo");
